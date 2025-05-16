47108b375b4807b6d459dc62f6b7add516367512
Chris <chris@clyates.plus.com>
2025-05-16 13:32:20 +0100

    Use Configuration trait

--- src/Node/DetailViewNode.php
+++ src/Node/DetailViewNode.php
@@ -6,23 +6,20 @@ namespace BeastBytes\Yii\DataView\Latte\Node;
 
 use Generator;
 use Latte\CompileException;
-use Latte\Compiler\Node;
 use Latte\Compiler\Nodes\AreaNode;
 use Latte\Compiler\Nodes\Php\ExpressionNode;
-use Latte\Compiler\Nodes\Php\FilterNode;
 use Latte\Compiler\Nodes\Php\IdentifierNode;
-use Latte\Compiler\Nodes\Php\ModifierNode;
 use Latte\Compiler\Nodes\StatementNode;
 use Latte\Compiler\PrintContext;
 use Latte\Compiler\Tag;
 
 class DetailViewNode extends StatementNode
 {
-    public ?ModifierNode $config = null;
-    public string $configuration = '';
+    use ConfigurationTrait;
+
     public ExpressionNode $data;
     public AreaNode $fields;
-    private IdentifierNode $name;
+    public IdentifierNode $name;
 
     /**
      * @throws CompileException
@@ -32,9 +29,8 @@ class DetailViewNode extends StatementNode
         $tag->expectArguments();
         $node = $tag->node = new self;
         $node->name = new IdentifierNode(ucfirst($tag->name));
-
         $node->data = $tag->parser->parseExpression();
-        $node->config = $tag->parser->parseModifier();
+        $node->configuration = $tag->parser->parseModifier();
 
         [$node->fields, $endTag] = yield;
         return $node;
@@ -42,59 +38,21 @@ class DetailViewNode extends StatementNode
 
     public function print(PrintContext $context): string
     {
-        $this->parseConfiguration($context);
-
         return $context->format(
             <<<'MASK'
-            echo 'Yiisoft\Yii\DataView\%node::widget()' %line;
-            echo "\n";
-            echo '    ->data(%node)';
-            echo "\n";
-            echo '    ->fields(';
-            %node
-            echo "\n";
-            echo '    )';
-            echo "\n";
-            echo '%raw';
-            echo ';';
+            echo Yiisoft\Yii\DataView\%node::widget() %line
+                ->data(%node)
+                ->fields(%node)
+            %raw
+            ;
             MASK,
             $this->name,
             $this->position,
             $this->data,
             $this->fields,
-            $this->configuration,
+            $this->parseConfiguration($context)
+
         );
-    }
-
-    private function parseConfiguration($context): void
-    {
-        $configuration = [];
-
-        /** @var FilterNode $config */
-        foreach ($this->config as $config) {
-            $name = '';
-            $atr = [];
-
-            foreach ($config as $c) {
-                if ($c instanceof IdentifierNode) {
-                    $name = (string) $c;
-                } else {
-                    $atr[] = $c;
-                }
-            }
-
-            if (empty($atr)) {
-                $configuration[$name] = '';
-            } else {
-                foreach ($atr as $a) {
-                    $configuration[$name] = $a instanceof Node ? $a->print($context) : '';
-                }
-            }
-        }
-
-        foreach ($configuration as $modifier => $value) {
-            $this->configuration .= "->$modifier($value)";
-        }
     }
 
     /**
