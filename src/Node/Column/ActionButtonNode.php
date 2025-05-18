5d49e4f43a2fab9911c70e7b9cfdd0038fbe712b
Chris <chris@clyates.plus.com>
2025-05-18 21:52:48 +0100

    Update node to extract button name

--- src/Node/Column/ActionButtonNode.php
+++ src/Node/Column/ActionButtonNode.php
@@ -6,20 +6,27 @@ namespace BeastBytes\Yii\DataView\Latte\Node\Column;
 
 use BeastBytes\Yii\DataView\Latte\Node\ArgumentTrait;
 use Generator;
+use Latte\Compiler\Nodes\Php\Expression\ArrayNode;
+use Latte\Compiler\Nodes\Php\IdentifierNode;
+use Latte\Compiler\Nodes\Php\Scalar\StringNode;
 use Latte\Compiler\Nodes\StatementNode;
 use Latte\Compiler\PrintContext;
 use Latte\Compiler\Tag;
 
-final class ActionButtonNode extends StatementNode
+class ActionButtonNode extends StatementNode
 {
     use ArgumentTrait;
+
+    public StringNode $buttonName;
+    public IdentifierNode $name;
 
     public static function create(Tag $tag): self
     {
         $tag->expectArguments();
         $node = $tag->node = new self;
+        $node->name = new IdentifierNode(ucfirst($tag->name));
         $node->arguments = $tag->parser->parseArguments();
-
+        $node->buttonName = array_shift($node->arguments->items)->value;
         return $node;
     }
 
@@ -27,8 +34,10 @@ final class ActionButtonNode extends StatementNode
     {
         return $context->format(
             <<<'MASK'
-            new Yiisoft\Yii\DataView\Column\ActionButton(%raw), %line
+            %node => new Yiisoft\Yii\DataView\Column\%node(%raw), %line
             MASK,
+            $this->buttonName,
+            $this->name,
             $this->parseArguments($context),
             $this->position,
         );
@@ -39,6 +48,8 @@ final class ActionButtonNode extends StatementNode
      */
     public function &getIterator(): Generator
     {
+        yield $this->buttonName;
+        yield $this->name;
         yield $this->arguments;
     }
 }
